32ce79f0ac055460bb9c4cb19afb7f525c280e0d
Eelis Roininen <eelis.roininen@hotmail.com>
2023-10-06 09:52:36 +0300

    Add rabbitMQ to compose, modify addresses, test with monitor get

--- monitor/index.ts
+++ monitor/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import { env } from 'bun'
+import express from 'express'
+const app = express()
+
+const PORT = env.PORT ?? 8087 
+
+app.use(express.json())
+
+const messages: string[] = ['Test']
+
+app.get('/', (_, res) => {
+  res.send(messages)
+  res.contentType('text/plain')
+  res.end()
+})
+
+app.listen(PORT)
+
+console.log('Server is running on port', PORT)
--- service2/index.ts
+++ service2/index.ts
@@ -3,10 +3,6 @@ import express from 'express'
 const app = express()
 
 const PORT = env.PORT ?? 8000
-const FILE_PATH = env.FILE_PATH ?? '../logs/service2.log'
-
-const file = Bun.file(FILE_PATH)
-const writer = file.writer()
 
 app.use(express.json())
 
@@ -14,7 +10,6 @@ app.post('/', (req, res) => {
   const message = req.body.message
 
   if (message === 'STOP') {
-    writer.end()
     res.end()
     server.close()
     return
@@ -23,9 +18,7 @@ app.post('/', (req, res) => {
   const originatingHost = req.body.origin
   const formattedMessage = `${message} ${originatingHost} \n`
   console.log(formattedMessage)
-  writer.write(formattedMessage)
   res.end()
-
 })
 
 const server = app.listen(PORT)
