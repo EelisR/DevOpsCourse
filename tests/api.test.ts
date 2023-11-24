9a4e45e3f50b6d408530fad64cf8feae14c7abc5
Eelis Roininen <eelis.roininen@hotmail.com>
2023-11-24 16:23:16 +0200

    Add API PUT tests

--- tests/api.test.ts
+++ tests/api.test.ts
@@ -12,16 +12,19 @@ beforeAll(() => {
   sleepSync(3000);
 });
 
+// Basic test
 test("should hello world", () => {
   const hello = "hello world";
   expect(hello).toBe("hello world");
 });
 
+// API Running
 test("API should respond with 200 to healthcheck", async () => {
   const res = await fetch(`http://${API}/healthcheck`);
   expect(res.status).toBe(200);
 });
 
+// Messages endpoint tests
 test("Messages should return with a text/plain content type", async () => {
   const res = await fetch(`http://${API}/messages`);
   expect(res.headers.get("content-type")).toBe("text/plain; charset=utf-8");
@@ -37,4 +40,54 @@ test("Messages should contain multiple lines of text", async () => {
   const text = await res.text();
   console.log(text);
   expect(text.split("\n").length).toBeGreaterThan(1);
+});
+
+// State endpoint tests
+test("API should respond with 200 to state", async () => {
+  const command = "RUNNING";
+  const res = await fetch(`http://${API}/state`, {
+    method: "PUT",
+    headers: { "Content-Type": "text/plain" },
+    body: command,
+  });
+
+  const responseState = await res.text();
+  expect(res.status).toBe(200);
+  expect(responseState).toBe(command);
+});
+
+test("API should respond with state RUNNING after INIT", async () => {
+  const res = await fetch(`http://${API}/state`, {
+    method: "PUT",
+    headers: { "Content-Type": "text/plain" },
+    body: "INIT",
+  });
+
+  const responseState = await res.text();
+  expect(res.status).toBe(200);
+  expect(responseState).toBe("RUNNING");
+});
+
+test("API should respond with state PAUSED after PAUSED", async () => {
+  const res = await fetch(`http://${API}/state`, {
+    method: "PUT",
+    headers: { "Content-Type": "text/plain" },
+    body: "PAUSED",
+  });
+
+  const responseState = await res.text();
+  expect(res.status).toBe(200);
+  expect(responseState).toBe("PAUSED");
+});
+
+test("API should not respond anymore after SHUTDOWN", async () => {
+  await fetch(`http://${API}/state`, {
+    method: "PUT",
+    headers: { "Content-Type": "text/plain" },
+    body: "SHUTDOWN",
+  });
+
+  sleepSync(3000);
+
+  expect(fetch(`http://${API}/state`)).toThrow();
 });
