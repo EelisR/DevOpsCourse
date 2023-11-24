5c771d6d6d020cac158f624468eec5a4d7cbf781
Eelis Roininen <eelis.roininen@hotmail.com>
2023-11-24 15:04:15 +0200

    Add healthcheck to api

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,45 @@
-console.log("Hello via Bun!");
+import { env, sleepSync } from "bun";
+import express from "express";
+import { exec } from "child_process";
+
+const PORT = env.PORT ?? 8083;
+
+const SERVICE_1_ADDRESS = env.SERVICE_1_ADDRESS ?? "10.0.1.3:4001";
+const MONITOR_ADDRESS = env.MONITOR_ADDRESS ?? "10.0.1.5:8087";
+const MQ_ADDRESS = env.MQ_ADDRESS ?? "10.1.2.2:5672";
+
+await waitForServices();
+
+startServer();
+
+function startServer() {
+  const app = express();
+  app.use(express.json());
+
+  app.get("/healthcheck", (_, res) => {
+    res.status(200);
+    res.send("OK");
+  });
+
+  app.listen(PORT);
+  console.log("Server is running on port", PORT);
+
+  return app;
+}
+
+async function waitForServices() {
+  const promises = [
+    waitForService(SERVICE_1_ADDRESS),
+    waitForService(MONITOR_ADDRESS),
+    waitForService(MQ_ADDRESS),
+  ];
+
+  await Promise.all(promises);
+}
+
+async function waitForService(service: string) {
+  console.log("Waiting for service to be ready...");
+  exec(`./wait-for-it/wait-for-it.sh ${service}`);
+  console.log("Service is ready!");
+  sleepSync(1000);
+}
